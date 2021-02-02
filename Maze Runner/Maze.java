091eb9e5aa15473341c4d8aa0b582e3debec3750
221016hkis <78184657+221016hkis@users.noreply.github.com>
2021-02-02 14:09:42 +0800

    Update Maze.java

--- Maze Runner/Maze.java
+++ Maze Runner/Maze.java
@@ -2,5 +2,7 @@ public class Maze {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("test 2");
+        
+        System.out.println("hello");
     }
 }

50c42131831bc91e4a6dac921c8a585339f147f2
Brian Chiang <briyanchiang@gmail.com>
2021-02-04 12:41:39 +0800

    made displayBoard and fixed makeBoard array size

--- Maze Runner/Maze.java
+++ Maze Runner/Maze.java
@@ -6,14 +6,31 @@ public class Maze {
         System.out.println("hello");
         
         System.out.println("test pull request");
+
+        makeBoard();
+
+        displayBoard();
+
     }
 
-    public void makeBoard(){
-        String[][] board = new String[2][2];
+    static String[][] board = new String[3][3];
+
+    static void makeBoard(){
         for (int row = 0; row < 3; row++){
             for (int column = 0; column < 3; column++){
                 board[row][column] = "-";
+
             }
+        }
+    }
+
+    static void displayBoard(){
+        for (int row = 0; row < 3; row++){
+            System.out.print(" | ");
+            for (int column = 0; column < 3; column++){
+                System.out.print(board[row][column] + " | ");
+            }
+            System.out.println();
         }
     }
 

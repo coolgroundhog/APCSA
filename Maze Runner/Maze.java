94728233b01bf991a5418d22e50f662752acd99a
Brian Chiang <briyanchiang@gmail.com>
2021-02-04 12:27:12 +0800

    created 2d array board

--- Maze Runner/Maze.java
+++ Maze Runner/Maze.java
@@ -9,4 +9,14 @@ public class Maze {
 
         System.out.println("pushing test6");
     }
+
+    public void makeBoard(){
+        String[][] board = new String[2][2];
+        for (int row = 0; row < 3; row++){
+            for (int column = 0; column < 3; column++){
+                board[row][column] = "-";
+            }
+        }
+    }
+    
 }
